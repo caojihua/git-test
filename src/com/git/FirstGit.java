ca18af3367bbfc414d90de0772d893e090350afd
cjh <1262371601@qq.com>
2020-06-22 21:05:05 +0800

    将本地工程添加到git

---
+++ src/com/git/FirstGit.java
@@ -0,0 +1,17 @@
+package com.git;
+
+public class FirstGit {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+
+    }
+}
